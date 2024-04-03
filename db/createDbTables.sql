dd2ed776ab2333c37788e718ac592aeac2348886
Katerina Vitik <katerinavitik@gmail.com>
2024-04-03 08:54:03 -0700

    added sql script for db schema

--- db/createDbTables.sql
+++ db/createDbTables.sql
@@ -1,18 +1,20 @@
-DROP TABLE IF EXISTS ministry;
+-- Create tables and corresponding relationships for ministry names, acronyms and histories
 
-CREATE TABLE IF NOT EXISTS CREATE TABLE IF NOT EXISTS ministry
+-- drop table if exists and add ministry table
+DROP TABLE IF EXISTS ministry;
+CREATE TABLE IF NOT EXISTS ministry
     (ministry_id SERIAL NOT NULL, 
     ministry_name VARCHAR(255) NOT NULL, 
     m_change_effective_date DATE NOT NULL,
     m_change_user VARCHAR(255) NOT NULL DEFAULT CURRENT_USER,
     m_datestamp DATE NOT NULL DEFAULT CURRENT_DATE,
-    is_current BOOLEAN NOT NULL
+    is_current BOOLEAN NOT NULL,
     PRIMARY KEY (ministry_id)
     );
 
+-- drop table if exists and add acronym table
 DROP TABLE IF EXISTS acronym;
-
-CREATE TABLE IF NOT EXISTS CREATE TABLE IF NOT EXISTS acronym
+CREATE TABLE IF NOT EXISTS acronym
     (acronym_id SERIAL NOT NULL,
     acronym VARCHAR(20) NOT NULL,
     a_change_effective_date DATE NOT NULL,
@@ -21,24 +23,23 @@ CREATE TABLE IF NOT EXISTS CREATE TABLE IF NOT EXISTS acronym
     PRIMARY KEY (acronym_id)
     );
 
+-- drop table if exists and add ministry_acronym table which will show the ministry with corresponding acronym
 DROP TABLE IF EXISTS ministry_acronym;
-CREATE TABLE IF NOT EXISTS CREATE TABLE IF NOT EXISTS ministry_acronym
-    (ministry_id references ministry(ministry_id),
-    acronym_id references acronym(acronym_id),
-    FOREIGN KEY (ministry_id)
+CREATE TABLE IF NOT EXISTS ministry_acronym
+    (ministry_id integer references ministry(ministry_id),
+    acronym_id integer references acronym(acronym_id),
+    PRIMARY KEY(ministry_id, acronym_id)
     );
 
+-- drop table if exists and add ministry_history table
 DROP TABLE IF EXISTS ministry_history;
-CREATE TABLE IF NOT EXISTS CREATE TABLE IF NOT EXISTS ministry_history
-    (ministry_id references ministry(ministry_id),
-    ministry_id_history references ministry(ministry_id)
+CREATE TABLE IF NOT EXISTS ministry_history
+    (ministry_id integer references ministry(ministry_id),
+    ministry_id_history integer references ministry(ministry_id),
+    acronym_id_history integer references acronym(acronym_id),
+    PRIMARY KEY(ministry_id, ministry_id_history)
     );
 
-DROP TABLE IF EXISTS acronym_history;
-CREATE TABLE IF NOT EXISTS CREATE TABLE IF NOT EXISTS acronym_history
-    (acronym_id references acronym(acronym_id),
-    acronym_id_history references acronym(acronym_id)
-    );
 
 
 
