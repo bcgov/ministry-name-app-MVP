e2ead67780e3897c06fd469c1d362e68d542d147
kat <katerinavitik@gmail.com>
2024-04-12 09:49:51 -0700

    add acronym option for ministry add

--- controllers/ministryController.js
+++ controllers/ministryController.js
@@ -1,18 +1,18 @@
 // dependecies
 const pool = require('../db/dbConnectionConfig.js');
-const {fetchData} = require('../utils/helperfunctions.js');
+const { fetchData } = require('../utils/helperfunctions.js');
 const asyncHandler = require("express-async-handler");
 const validator = require("express-validator");
 
 // sql query 
-const queryMinistry='SELECT m.ministry_id, m.ministry_name, a.acronym, m.m_change_effective_date, m.is_current FROM ministry m JOIN ministry_acronym ma ON m.ministry_id = ma.ministry_id JOIN acronym a ON a.acronym_id = ma.acronym_id ORDER BY m.ministry_name ASC;'
+const queryMinistry = 'SELECT m.ministry_id, m.ministry_name, a.acronym, m.m_change_effective_date, m.is_current FROM ministry m JOIN ministry_acronym ma ON m.ministry_id = ma.ministry_id JOIN acronym a ON a.acronym_id = ma.acronym_id ORDER BY m.ministry_name ASC;'
 
 // get ministry data and render to index view
 const getMinistryData = asyncHandler(async (req, res) => {
   try {
     // Call fetchData to retrieve data from the db
-    const theData = await fetchData(pool, queryMinistry);  
-    res.render('index', { title: 'Ministry Names', data: theData.rows});
+    const theData = await fetchData(pool, queryMinistry);
+    res.render('index', { title: 'Ministry Names', data: theData.rows });
   } catch (err) {
     console.error('Error fetching ministry data:', err);
     res.status(500).json({ error: 'Internal server error' });
@@ -20,15 +20,24 @@ const getMinistryData = asyncHandler(async (req, res) => {
 });
 
 // get ministry data from form and post to DB to create new ministry
-const addMinistry = asyncHandler(async (req,res)=>{
+const addMinistry = asyncHandler(async (req, res) => {
   try {
     let minName = req.body.ministryName;
-    //let acronym = req.body.acronym;
     let effectiveDate = req.body.effectiveDate;
-    console.log(`name: ${minName}, date: ${effectiveDate}`);
+    let acronym;
+    let acronymDate;
 
+    if (req.body.acronymChoice === "true") {// if yes radio is selected
+      acronym = req.body.acronym;
+      acronymDate = req.body.acronymDate;
+      console.log(`name: ${minName}, date: ${effectiveDate}, acronym: ${acronym}, acronym date: ${acronymDate} `);
+
+    } else {
+      console.log(`name: ${minName}, date: ${effectiveDate}`);
+    }
+    res.redirect('/');
   } catch (err) {
-    console.err('Error creating new Ministry:', err);
+    console.error('Error creating new Ministry:', err);
     res.status(500).json({ error: 'Internal server error' });
   }
 });
@@ -36,4 +45,4 @@ const addMinistry = asyncHandler(async (req,res)=>{
 
 
 
-    module.exports = {getMinistryData, addMinistry};
+module.exports = { getMinistryData, addMinistry };
--- public/script/custom.js
+++ public/script/custom.js
@@ -1,12 +1,14 @@
 //______________________Globals_______________
 
-    //create history array
-    let theHistory =[];
+//create history array
+let theHistory = [];
 
 //______________________Function Definitions_______________
 
-   // function to populate history array with histories
-   let getHistory = (jsonObj, id) =>{
+
+
+// function to populate history array with histories
+const getHistory = (jsonObj, id) => {
     // convert id into number
     let idNum = Number(id);
 
@@ -14,26 +16,55 @@
     let ministry = jsonObj.find((myMinistry) => myMinistry.ministry_id == idNum);
 
     // recursivly find the previous ministry history until the ministry_id_history # is null
-    if(ministry.ministry_id_history === null){
+    if (ministry.ministry_id_history === null) {
         theHistory.push(ministry); // adds itself to the array
         return history;
     }
-    else{
+    else {
         theHistory.push(ministry); // add object to theHistory array
         let newHistId = ministry.ministry_id_history;
         getHistory(jsonObj, newHistId);
-    };    
+    };
 }
 
+const toggleAcronymSection = () => {
+    // Dom elements into variables
+    const isAcronymYes = document.getElementById('acronymYes').checked;
+    const acronymDiv = document.getElementById('reveal-if-active');
+    const acronymInput = document.getElementById('acronym');
+    const acronymDateInput = document.getElementById('acronymDate');
+
+    // if 'yes' radio selected, Acronym and acronym date inputs are required
+    if (isAcronymYes) {
+        acronymDiv.style.display = 'block';
+        acronymInput.required = true;
+        acronymDateInput.required = true;
+    } else {
+        acronymDiv.style.display = 'none';
+        acronymInput.required = false;
+        acronymDateInput.required = false;
+    }
+};
+
+//______________________Acronym display based on radio selection functionality_______________
+
+document.addEventListener('DOMContentLoaded', () => {
+    //When radio button selection changes, show or hide Acronym section
+    document.getElementById('acronymYes').addEventListener('change', toggleAcronymSection);
+    document.getElementById('acronymNo').addEventListener('change', toggleAcronymSection);
+
+    // Initial state setup
+    toggleAcronymSection();
+});
 
 //______________________History functionality on button click_______________
 
 //When submit button is clicked, get ministry selected and display that ministrys's history
-document.getElementById('historyForm').addEventListener('submit', async (event)=> {
-    
+document.getElementById('historyForm').addEventListener('submit', async (event) => {
+
     //stop the page from reloading when form is submitted:
     event.preventDefault();
-    
+
     // get ministry_id selected:
     const minId = document.getElementById('selectMinistryHist').value;
 
@@ -44,21 +75,21 @@ document.getElementById('historyForm').addEventListener('submit', async (event)=
     getHistory(theData, minId);
 
     // clear table if its already displaying data
-    document.getElementById("historyTable").innerHTML ="";
-    document.getElementById("noHistory").innerHTML ="";
+    document.getElementById("historyTable").innerHTML = "";
+    document.getElementById("noHistory").innerHTML = "";
 
     //diplay the history data
-let historySetup =`
+    let historySetup = `
     <tr>
         <th>Ministry Name</th>
         <th>Acronym</th>
         <th>Effective Date</th> 
     </tr>
 `
-    if (theHistory.length <= 1){
+    if (theHistory.length <= 1) {
         document.querySelector("p#noHistory").insertAdjacentHTML("afterbegin", "No history available");
-    } else{
-        document.querySelector("table").insertAdjacentHTML("beforeend", historySetup); 
+    } else {
+        document.querySelector("table").insertAdjacentHTML("beforeend", historySetup);
         theHistory.forEach(element => {
             let eachMinistry = ` <tr>
                 <td>${element.ministry_name}</td>
@@ -66,9 +97,9 @@ let historySetup =`
                 <td>${element.m_change_effective_date}</td>
                 </tr>
             `
-            document.querySelector("table").insertAdjacentHTML("beforeend", eachMinistry); 
+            document.querySelector("table").insertAdjacentHTML("beforeend", eachMinistry);
         });
     }
     //clear theHistory array
-    theHistory =[];
+    theHistory = [];
 });
