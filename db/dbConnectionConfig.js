719dea01d7104e19d98df810da11b07d6e0526a3
Katerina Vitik <katerinavitik@gmail.com>
2024-05-31 11:09:07 -0700

    updates to postgres dockerfile, dockercompose and openshipt.yml

--- db/dbConnectionConfig.js
+++ db/dbConnectionConfig.js
@@ -7,7 +7,8 @@ const pool = new Pool({
   host: process.env.POSTGRES_HOST,
   user: process.env.POSTGRES_USER ,
   database: process.env.POSTGRES_DB,
-  password: process.env.POSTGRES_PASSWORD
+  password: process.env.POSTGRES_PASSWORD,
+  //port: process.env.PORT
 });
 
 module.exports = pool;
