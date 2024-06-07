6723466d9333ead9da54a580e86362545336a9fb
Katerina Vitik <katerinavitik@gmail.com>
2024-06-07 08:55:15 -0700

    change app prot to 8080 + Ci pipeline trouble-shooting

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const helperfunctions = require('./utils/helperfunctions.js')
 require('dotenv').config({path: '../.env'});
 
 // get port from .env file and load into expressh
-const port = helperfunctions.normalizePort(process.env.PORT || '3000');
+const port = helperfunctions.normalizePort(process.env.PORT || '8080');
 app.set('port', process.env.PORT);
 console.log(process.env.POSTGRES_HOST);
 
