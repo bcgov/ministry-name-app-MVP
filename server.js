400b44ed280c1eb6003d5382d33c4e9427a5b4ae
Katerina Vitik <katerinavitik@gmail.com>
2024-06-19 09:53:07 -0700

    changes to server.js file

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 //import dependencies
 const app = require('./app');
-const http = require('http');
+//const http = require('http');
 const helperfunctions = require('./utils/helperfunctions.js')
 require('dotenv').config({path: '../.env'});
 
@@ -10,10 +10,10 @@ app.set('port', process.env.PORT);
 console.log(process.env.POSTGRES_HOST);
 
 // Create HTTP server.
-const server = http.createServer(app);
+//const server = http.createServer(app);
 
 // Listen on provided port
-server.listen(port, ()=> 
+app.listen(port, ()=> 
   console.log(`App is running on localhost:${port}`)
 );
-server.on('error', helperfunctions.onError);
+app.on('error', helperfunctions.onError);
